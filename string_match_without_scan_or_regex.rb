fa138ccc59e371620f1757638831d473a747534e
ymoses <ymoses@extendmed.com>
2016-02-05 10:41:17 -0600

    added additional check

--- string_match_without_scan_or_regex.rb
+++ string_match_without_scan_or_regex.rb
@@ -9,13 +9,18 @@ def match?(string, substring)
 end
 
 def find_character_match(string_array, substring_array, substring_character, substring_index, character_matches)
-  if substring_index == 0 && string_array.include?(substring_character)
-    character_matches.push(substring_character)
-  elsif substring_index > 0 && string_array.include?(substring_character)
-    string_array.each_with_index do |string_character, string_index|
-      if string_character == substring_character && string_array[string_index - 1] == substring_array[substring_index - 1]
-        character_matches.push(substring_character)
-        break
+  if string_array.include?(substring_character)
+    if substring_index == 0
+      character_matches.push(substring_character)
+    elsif substring_index > 0
+      string_array.each_with_index do |string_character, string_index|
+        characters_match = string_character == substring_character
+        previous_string_index_not_negative = string_index - 1 > -1
+        previous_characters_match = string_array[string_index - 1] == substring_array[substring_index - 1]
+        if characters_match && previous_string_index_not_negative && previous_characters_match
+          character_matches.push(substring_character)
+          break
+        end
       end
     end
   end
@@ -26,3 +31,4 @@ p match?("muchlikes", "like") == true
 p match?("ikelmuchlikes", "like") == true
 p match?("bass1lurv", " 1lurv") == false
 p match?("1basslurv", "1lurv") == false
+p match?("ikemarl", "like") == false
