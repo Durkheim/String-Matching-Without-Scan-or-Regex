501cd4bb9bec2b59b03a511fadd1b9767ff6f5cc
Yiannis Moses <yiannis.moses@Centro-103794.centro.net>
2016-07-06 17:38:32 -0500

    creating class specific match with recursion

--- string_match_without_scan_or_regex.rb
+++ string_match_without_scan_or_regex.rb
@@ -1,33 +1,69 @@
-def match?(string, substring)
-  string_array = string.split("")
-  substring_array = substring.split("")
-  character_matches = []
-  substring_array.each_with_index do |substring_character, substring_index|
-    find_character_match(string_array, substring_array, substring_character, substring_index, character_matches)
+# def match?(string, substring)
+#   string_array = string.split("")
+#   substring_array = substring.split("")
+#   character_matches = []
+#   substring_array.each_with_index do |substring_character, substring_index|
+#     find_character_match(string_array, substring_array, substring_character, substring_index, character_matches)
+#   end
+#   character_matches.join("") == substring ? true : false
+# end
+#
+# def find_character_match(string_array, substring_array, substring_character, substring_index, character_matches)
+#   if string_array.include?(substring_character)
+#     if substring_index == 0
+#       character_matches.push(substring_character)
+#     elsif substring_index > 0
+#       string_array.each_with_index do |string_character, string_index|
+#
+#         characters_do_match = string_character == substring_character
+#         previous_string_index_not_negative = string_index - 1 > -1
+#         previous_characters_do_match = string_array[string_index - 1] == substring_array[substring_index - 1]
+#
+#         if characters_do_match && previous_string_index_not_negative && previous_characters_do_match
+#           character_matches.push(substring_character)
+#           break
+#         end
+#       end
+#     end
+#   end
+# end
+
+# I. :match? method to
+
+class StringMatcher
+
+  def match?(string, substring)
+    matches = []
+    matches = generate_matches(string, substring, matches)
+    matches.include?(substring)
   end
-  character_matches.join("") == substring ? true : false
-end
 
-def find_character_match(string_array, substring_array, substring_character, substring_index, character_matches)
-  if string_array.include?(substring_character)
-    if substring_index == 0
-      character_matches.push(substring_character)
-    elsif substring_index > 0
-      string_array.each_with_index do |string_character, string_index|
+  def match_count
+    matches = []
+    matches = generate_matches(string, substring, matches)
+    matches.length
 
-        characters_do_match = string_character == substring_character
-        previous_string_index_not_negative = string_index - 1 > -1
-        previous_characters_do_match = string_array[string_index - 1] == substring_array[substring_index - 1]
+  end
 
-        if characters_do_match && previous_string_index_not_negative && previous_characters_do_match
-          character_matches.push(substring_character)
-          break
-        end
-      end
+  private
+
+  def generate_matches(string, substring, matches)
+
+
+    string_slice = string.slice(0, substring.length)
+
+    if string_slice == substring
+      matches.push(string_slice)
+    elsif string.length > 1
+      new_string_array = string.split('')
+      new_string_array.shift
+      new_string = new_string_array.join('')
+      match?(new_string, substring, matches)
     end
-  end
-end
 
+  end
+
+end
 p match?("likes", "like") == true
 p match?("muchlikes", "like") == true
 p match?("ikelmuchlikes", "like") == true
