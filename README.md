23841f97b640a3bcfe67f4aa3c9d6bc28e0a2564
Yiannis Moses <yiannis.moses@Centro-103794.centro.net>
2016-07-08 15:02:38 -0500

    updating specs with different data types

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# String Matching Without Scan or Regex
+# Matching Without Scan or Regex
 
 ### Objective
 
 Without using :scan or RegEx perform the following:
 
-- write a :match? method that returns true or false if a substring is found within a string
+- write a :match? method that returns true or false if a target object is found within a candidate object
 - write a :match_count method that returns an integer value for the number of matches
-- write a :match_indices method that returns an array of integers, indicating indices where matches in the string occur
+- write a :match_indices method that returns an array of integers, indicating indices where matches in the candidate object occur
+
+<b>Note:</b>
+
+Initially, the purpose of this exercise was to write a match method for string objects. However, it has been extended to be a match for several data types.
 
 ### Requirements
 
@@ -14,17 +18,14 @@ gem install rspec
 
 ### Pseudocode
 
-I. Write a method solely dedicated to generating a list of matches through recursion<br/>
-II. Add the class methods :match?, :match_count, and :match_indices to String. Each method should reference the method used to generate the list of matches.<br/>
-III. Raise an exception error if the argument passed in for each method is not a valid type<br/>
+I. Create a Matcher class
+II. Write a method solely dedicated to generating a list of matches through recursion<br/>
+III. Add the class methods :match?, :match_count, and :match_indices to Matcher<br/>
+    - Each method should reference the method used to generate the list of matches.<br/>
+    - the matching methods should accommodate a range of data types
 
 ###Solution
 
-To recursively generate a list of matches, the original string was sliced from the 0 index to the length of the substring. The slice was then compared to the substring.
-If the two matched, the slice was then collected in a hash, along with the index at which the match occurred in the original string. This process was done until
-the original string was sliced down to a length of 1, at which point the method was terminated and the list of matches was returned. The implementation to generate the list of matches
-was kept private, so that objects and methods outside the class would not have access, since, on an interface level, it's only necessary to determine if there is a match, the match count,
-and where the matches occurred. The method for generating the list was called :generate_matches.
 
 
 The methods :match?, :match_count, and :match_indices all reference :generate_matches and interact with generated list to give each desired return. A spec was created to
--- string_match_without_scan_or_regex_spec.rb
+++ string_match_without_scan_or_regex_spec.rb
@@ -4,27 +4,36 @@ describe Matcher do
 
   let(:matcher){Matcher.new}
 
+
   let(:randomly_generated_strings){ (0..2).map{(0..rand(1..100)).map{(rand(127)).chr}.join}}
-  let(:string){ randomly_generated_strings[0] + "like" + randomly_generated_strings[1] + "like" + randomly_generated_strings[2]}
-  let(:substring_with_match){"like"}
-  let(:substring_with_no_match){"nope"}
+  let(:candidate_string){ randomly_generated_strings[0] + "like" + randomly_generated_strings[1] + "like" + randomly_generated_strings[2]}
+  let(:target_string_with_match){"like"}
+  let(:target_string_with_no_match){"nope"}
+
+  let(:candidate_array){[[1,3], 1]}
+  let(:target_array_with_match){[1,3]}
+  let(:target_array_with_no_match){[]}
+
+  let(:candidate_hash){{a: 1, b: 2}}
+  let(:target_hash_with_match){{a: 1, b: 2}}
+  let(:target_hash_with_no_match){{}}
 
 
   #Testing #match? functionality for objects of the class String
   describe '#match?' do
 
-    it 'should be a defined method on the class String' do
-      expect(String.method_defined?(:match?)).to be(true)
-      #a test against false positives for defined methods
-      expect(String.method_defined?(:matchez?)).to be(false)
-    end
-
     it 'should return true when a match is found' do
-      expect(string.match?(substring_with_match)).to be(true)
+      expect(matcher.match?(candidate_string, target_string_with_match)).to be(true)
+      expect(matcher.match?(candidate_array, target_array_with_match)).to be(true)
+      expect(matcher.match?(candidate_hash, target_hash_with_match)).to be(true)
     end
 
     it 'should return false when a match is not found' do
-      expect(string.match?(substring_with_no_match)).to be(false)
+      expect(matcher.match?(candidate_string, target_string_with_no_match)).to be(false)
+      expect(matcher.match?(candidate_array, target_array_with_no_match)).to be(false)
+      expect(matcher.match?(candidate_hash, target_hash_with_no_match)).to be(false)
+
+
     end
 
   end
@@ -33,17 +42,11 @@ describe Matcher do
   #Testing #match_count functionality for objects of the class String
   describe '#match_count' do
 
-    it 'should be a defined method on the class String' do
-      expect(String.method_defined?(:match_count)).to be(true)
-      #a test against false positives for defined methods
-      expect(String.method_defined?(:match_countz)).to be(false)
-    end
-
     it 'should return an integer for the number of matches' do
-      expect(string.match_count(substring_with_match)).to be_kind_of(Integer)
-      expect(string.match_count(substring_with_match)).to eq(2)
-      expect(string.match_count(substring_with_no_match)).to be_kind_of(Integer)
-      expect(string.match_count(substring_with_no_match)).to eq(0)
+      expect(matcher.match_count(candidate_string, target_string_with_match)).to be_kind_of(Integer)
+      expect(matcher.match_count(candidate_string, target_string_with_match)).to eq(2)
+      expect(matcher.match_count(candidate_string, target_string_with_no_match)).to be_kind_of(Integer)
+      expect(matcher.match_count(candidate_string, target_string_with_no_match)).to eq(0)
     end
 
   end
@@ -51,21 +54,15 @@ describe Matcher do
 
   #Testing #match_indices functionality for objects of the class String
   describe '#match_indices' do
-    let(:new_string){"muchlikeslike"}
-    let(:new_substring_with_match){"like"}
+    let(:new_candidate_string){"muchlikeslike"}
+    let(:new_target_string_with_match){"like"}
 
-    it 'should be a defined method on the class String' do
-      expect(String.method_defined?(:match_indices)).to be(true)
-      #a test against false positives for defined methods
-      expect(String.method_defined?(:match_indicez)).to be(false)
-    end
-
-    it 'should return an array of integers indicating indices of a match' do
-      expect(string.match_indices(substring_with_match)).to be_kind_of(Array)
+    it 'should return an array of integers if there is at least one match' do
+      expect(matcher.match_indices(candidate_string, target_string_with_match)).to be_kind_of(Array)
       #using new_string and new_substring to easily verify position
-      expect(new_string.match_indices(new_substring_with_match)).to eq([4,9])
-      expect(string.match_indices(substring_with_no_match)).to be_kind_of(Array)
-      expect(string.match_indices(substring_with_no_match)).to eq([])
+      expect(matcher.match_indices(new_candidate_string, new_target_string_with_match)).to eq([4,9])
+      expect(matcher.match_indices(candidate_string, target_string_with_no_match)).to be_kind_of(Array)
+      expect(matcher.match_indices(candidate_string, target_string_with_no_match)).to eq([])
     end
 
   end
