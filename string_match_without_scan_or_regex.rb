3d29d21f50634c5f3b724fadacdd6c8f6c9ffa50
Yiannis Moses <yiannis.moses@Centro-103794.centro.net>
2016-07-12 12:41:04 -0500

    finalizing iterative method

--- string_match_without_scan_or_regex.rb
+++ string_match_without_scan_or_regex.rb
@@ -1,32 +1,35 @@
 class Matcher
   class << self
-    def match?(candidate, target)
 
-      matches = generate_matches(candidate, target, [])
+    def recursive_match?(candidate, target)
+      matches = generate_matches_recursive(candidate, target, [])
       matches.include?(target.to_s)
     end
 
-    def match_count(candidate, target)
-      matches = generate_matches(candidate, target, [])
-      matches.count(target.to_s)
-    end
-
-    #only returns indices for strings
-    def match_indices_for_strings(candidate, target)
-      matches = generate_matches(candidate, target, [])
-      matches.each_index.select{|index| matches[index] == target}
+    def iterative_match?(candidate, target)
+      matches = generate_matches_iterative(candidate, target)
+      matches.include?(target.to_s)
     end
 
     private
 
     #for first pass, collection_of_matches should be []
-    def generate_matches(candidate, target, collection_of_matches)
+    def generate_matches_recursive(candidate, target, collection_of_matches)
       candidate_string = candidate.to_s
       target_string = target.to_s
       candidate_string.start_with?(target_string) ? collection_of_matches.push(target_string) : collection_of_matches.push(nil)
       return collection_of_matches if candidate_string.empty?
       next_string_section = candidate_string.slice(1, candidate_string.length)
-      generate_matches(next_string_section, target_string, collection_of_matches)
+      generate_matches_recursive(next_string_section, target_string, collection_of_matches)
     end
+
+    def generate_matches_iterative(candidate, target)
+      candidate_string = candidate.to_s
+      target_string = target.to_s
+      candidate_string.split("").each_index.map do |index|
+        target_string if candidate_string.slice(index, target_string.length).start_with?(target_string)
+      end
+    end
+
   end
 end
